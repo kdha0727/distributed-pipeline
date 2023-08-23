2502d75d1af7b471ed45c8d1f5b725c18042f347
kdha0727 <dhakim@yonsei.ac.kr>
2023-08-24 01:25:45 +0900

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,38 +1,43 @@
 def load_data_from_args(
         split,
         data_dir,
-        batch_size,
-        # TODO: add args on your own
-        deterministic=False,
-        loop=True,
-        num_loader_proc=1,
+        # TODO: add dataset args on your own
+        batch_size: int,
+        deterministic: bool = False,
+        loop: bool = True,
+        seed: ... = 0,
+        window_size: float = 0.5,
+        num_loader_proc: int = 1,
 ):
+    from basic_utils import dist_util
+    from basic_utils.sampler import InfiniteSampler
+    from torch.utils.data.distributed import DistributedSampler
     from torch.utils.data import DataLoader
     from .dataset import CustomDataset
 
-    # TODO: add your data-loading function from split & data_dir & your arguments
-    dataset_kwargs = ...
+    rank = dist_util.get_rank()
+    num_replicas = dist_util.get_world_size()
 
-    data = CustomDataset(**dataset_kwargs)
-    loader = DataLoader(
-        data,
-        batch_size=batch_size,
+    # TODO: add your data-loading function from split & data_dir & your arguments
+    dataset_kwargs = dict(data_dir=data_dir, split=split)  # TODO
+    dataset = CustomDataset(**dataset_kwargs)
+
+    sampler_kwargs = dict(
+        num_replicas=num_replicas,
+        rank=rank,
         shuffle=not deterministic,
-        num_workers=num_loader_proc,
-        persistent_workers=num_loader_proc > 0,
+        seed=hash(seed)
     )
     if loop:
-        return infinite_loader_from_iterable(loader)
+        sampler = InfiniteSampler(dataset, **sampler_kwargs, window_size=window_size)
     else:
-        return loader
+        sampler = DistributedSampler(dataset, **sampler_kwargs, drop_last=False)
 
-
-def infinite_loader_from_object(obj):
-    import copy
-    while True:
-        yield copy.deepcopy(obj)
-
-
-def infinite_loader_from_iterable(iterable):
-    while True:
-        yield from iterable
+    return DataLoader(
+        dataset,
+        batch_size=batch_size // num_replicas,
+        sampler=sampler,
+        num_workers=num_loader_proc,
+        persistent_workers=num_loader_proc > 0,
+        # pin_memory=True,
+    )
